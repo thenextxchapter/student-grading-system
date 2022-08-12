2b049835539559f70cda8736e0829de07d1247da
chinonso ata <chinonso.ata@gmail.com>
2022-08-12 17:53:20 +0100

    Initial commit

---
+++ src/main/java/com/nony/studentgradingsystem/StudentGradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nony.studentgradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nony/studentgradingsystem/StudentGradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nony.studentgradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
