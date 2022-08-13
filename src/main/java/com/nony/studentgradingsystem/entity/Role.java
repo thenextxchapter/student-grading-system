3dc8119311ccf475242ca590c85d3ce0715e97fa
chinonso ata <chinonso.ata@gmail.com>
2022-08-13 10:24:06 +0100

    Created both Role and User entities and added lombok dependency

---
+++ src/main/java/com/nony/studentgradingsystem/entity/Role.java
@@ -0,0 +1,69 @@
+package com.nony.studentgradingsystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@Column(length = 40, nullable = false, unique = true)
+	private String name;
+
+	@Column(length = 150, nullable = false)
+	private String description;
+
+	public Role(Integer id) {
+		this.id = id;
+	}
+
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
---
+++ src/main/java/com/nony/studentgradingsystem/entity/User.java
@@ -0,0 +1,78 @@
+package com.nony.studentgradingsystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 128, nullable = false, unique = true)
+	private String email;
+
+	@Column(length = 64, nullable = false)
+	private String password;
+
+	@Column(name = "first_name", length = 45, nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", length = 45, nullable = false)
+	private String lastName;
+
+	@Column(length = 64)
+	private String photo;
+	private boolean enabled;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
+	private Set<Role> roles = new HashSet<>();
+
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", email='" + email + '\'' +
+				", password='" + password + '\'' +
+				", firstName='" + firstName + '\'' +
+				", lastName='" + lastName + '\'' +
+				", roles=" + roles +
+				'}';
+	}
+
+	@Transient
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+}
