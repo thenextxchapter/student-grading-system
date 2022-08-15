74b19bb54bc321538fc179983582de367793f4d1
chinonso ata <chinonso.ata@gmail.com>
2022-08-15 17:58:50 +0100

    Handled Rest Controller for checking for duplicate email

---
+++ src/main/java/com/nony/studentgradingsystem/controller/UserRestController.java
@@ -0,0 +1,18 @@
+package com.nony.studentgradingsystem.controller;
+
+import com.nony.studentgradingsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+	@Autowired
+	private UserService service;
+
+	@PostMapping("/users/check-email")
+	public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email) {
+		return service.isEmailUnique(id, email) ? "OK" : "Duplicated";
+	}
+}
---
+++ src/main/java/com/nony/studentgradingsystem/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nony.studentgradingsystem.exception;
+
+public class UserNotFoundException extends Exception{
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
