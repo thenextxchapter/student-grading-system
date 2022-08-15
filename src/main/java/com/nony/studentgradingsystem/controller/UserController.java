e8f4c5cd080ff44104989d6f7b429d8d96fe6b30
chinonso ata <chinonso.ata@gmail.com>
2022-08-15 17:53:41 +0100

    Created methods in the service and controller methods to save new users and hash passwords

--- src/main/java/com/nony/studentgradingsystem/controller/UserController.java
+++ src/main/java/com/nony/studentgradingsystem/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.nony.studentgradingsystem.controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import com.nony.studentgradingsystem.entity.Role;
 import com.nony.studentgradingsystem.entity.User;
 import com.nony.studentgradingsystem.service.UserService;
+import com.nony.studentgradingsystem.utils.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
@@ -21,5 +29,45 @@ public class UserController {
 
 		model.addAttribute("userList", usersList);
 		return "users/users";
+	}
+
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		User user = new User();
+		user.setEnabled(true);
+
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
+		model.addAttribute("headerTitle", "New User");
+
+		return "users/user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(
+			User user,
+			RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile
+	) throws IOException {
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhoto(fileName);
+			User savedUser = service.save(user);
+
+			String uploadDir = "../user-photos/" + savedUser.getId();
+
+			FileUploadUtil.cleanDir(uploadDir);
+
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if (user.getPhoto().isEmpty()) user.setPhoto(null);
+			service.save(user);
+		}
+
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
+		return "redirect:/users";
 	}
 }
--- src/main/java/com/nony/studentgradingsystem/service/UserService.java
+++ src/main/java/com/nony/studentgradingsystem/service/UserService.java
@@ -1,5 +1,6 @@
 package com.nony.studentgradingsystem.service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 import com.nony.studentgradingsystem.entity.Role;
@@ -7,9 +8,11 @@ import com.nony.studentgradingsystem.entity.User;
 import com.nony.studentgradingsystem.repository.RoleRepository;
 import com.nony.studentgradingsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class UserService {
 
 	@Autowired
@@ -18,6 +21,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepo;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	public List<User> listAll() {
 		return (List<User>) userRepo.findAll();
 	}
@@ -25,4 +31,44 @@ public class UserService {
 	public List<Role> listRoles(){
 		return (List<Role>) roleRepo.findAll();
 	}
-}
+
+	public User save(User user) {
+		boolean isUpdatingUser = (user.getId() != null);
+
+		if (isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+
+			if (user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			} else {
+				encodePassword(user);
+			}
+		} else {
+			encodePassword(user);
+		}
+
+		return userRepo.save(user);
+	}
+
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+	}
+
+	public boolean isEmailUnique(Integer id, String email) {
+		User userByEmail = userRepo.getUserByEmail(email);
+
+		if (userByEmail == null) return true;
+
+		boolean isCreatingNew = (id == null);
+
+		if (isCreatingNew) {
+			if (userByEmail != null) return false;
+		} else {
+			if (userByEmail.getId() != id) {
+				return false;
+			}
+		}
+
+		return true;
+	}}
